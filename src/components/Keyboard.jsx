75f84b33ef83ce4f3d6662a384e02583fba8dc13
RosaBen <benchabane.rosa@outlook.com>
2026-07-29 19:59:38 +0200

    added logic displaying letters when is correct

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -2,14 +2,12 @@ import clsx from "clsx";
 
 export default function Keyboard({ onSelect, selected, currentWord }) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
   const lowercaseWord = currentWord.toLowerCase();
 
   const keyboardsLetters = alphabet.split("").map((letter) => {
-    const isSelected = selected.includes(letter.toLowerCase());
-    const isCorrect =
-      isSelected && lowercaseWord.includes(letter.toLowerCase());
-    const isWrong = isSelected && !lowercaseWord.includes(letter.toLowerCase());
+    const isSelected = selected.includes(letter);
+    const isCorrect = isSelected && lowercaseWord.includes(letter);
+    const isWrong = isSelected && !lowercaseWord.includes(letter);
     const className = clsx({
       correct: isCorrect,
       wrong: isWrong,
